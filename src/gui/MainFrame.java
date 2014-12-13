6f259f304a6f852ead28d70e79c124ba1d6b88c2
Antoine De Gieter <antoine.degieter@net-production.ch>
2014-12-13 20:37:58 +0100

    Organize imports

--- src/gui/MainFrame.java
+++ src/gui/MainFrame.java
@@ -1,27 +1,18 @@
 package gui;
 
 import java.awt.BorderLayout;
-import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Font;
-import java.awt.Point;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
-import java.util.Arrays;
 
-import javax.swing.JButton;
-import javax.swing.JComboBox;
 import javax.swing.JFrame;
-import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTabbedPane;
 import javax.swing.JTextField;
 
 import app.Race;
-import app.Year;
 import data.DataManager;
 
 @SuppressWarnings("serial")
@@ -38,7 +29,7 @@ public class MainFrame extends JFrame implements KeyListener {
 	private final static Font FONT = new Font("Lato", Font.PLAIN, 12);
 
 	public MainFrame() {
-		super("Trans-Jurassienne | BÃ©ligat et De Gieter");
+		super("Trans-Jurassienne | BŽligat et De Gieter");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setSize(new Dimension(800, 600));
 		setLocationRelativeTo(null);
