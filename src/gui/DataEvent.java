f6c9d9d1b1bff0a97a276cb563bf5a04c27f1482
MrPwet <beligat.fx@gmail.com>
2014-12-13 19:49:19 +0100

    Ups, I forgot to add those two classes

---
+++ src/gui/DataEvent.java
@@ -0,0 +1,37 @@
+package gui;
+
+public class DataEvent {
+	private String year;
+	private String raceCat;
+	private String search;
+	
+	public DataEvent(String year, String raceCat, String search) {
+		this.year = year;
+		this.raceCat = raceCat;
+		this.search = search;
+	}
+
+	public String getYear() {
+		return year;
+	}
+
+	public void setYear(String year) {
+		this.year = year;
+	}
+
+	public String getRaceCat() {
+		return raceCat;
+	}
+
+	public void setRaceCat(String raceCat) {
+		this.raceCat = raceCat;
+	}
+
+	public String getSearch() {
+		return search;
+	}
+
+	public void setSearch(String search) {
+		this.search = search;
+	}
+}
---
+++ src/gui/DataListener.java
@@ -0,0 +1,5 @@
+package gui;
+
+public interface DataListener {
+	public void dataEmitted(DataEvent e);
+}
